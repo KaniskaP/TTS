8557c9f20928913af44e8f187b251a0d5ef01938
Kan!skA <kaniska2008@gmail.com>
2015-05-12 10:09:06 +0530

    build2

--- Gujarati/DMOS.py
+++ Gujarati/DMOS.py
@@ -17,6 +17,7 @@ class TTS(unittest.TestCase):
         time.sleep(11)
         cls.driver.maximize_window()
         cls.driver.get("http://intranet.daiict.ac.in:8085/DonlabTTS/")
+        time.sleep(5)
 
     def test_TTS(self):
         # Change Your Set Number Here
@@ -29,20 +30,21 @@ class TTS(unittest.TestCase):
         for row in reader:
             rows[lineNum] = row
             lineNum += 1
+        time.sleep(2)
         for inputLineNum, inputData in rows.items():
             # Click on Clear Button
             self.driver.find_element_by_xpath(".//*[@id='AutoNumber1']/tbody/tr[4]/td/input[1]").click()
-            time.sleep(1)
+            time.sleep(5)
             # Enter Input in the Text Area
             self.driver.find_element_by_xpath(".//*[@id='AutoNumber1']/tbody/tr[7]/td[2]/p/textarea").send_keys(inputData)
-            time.sleep(2)
+            time.sleep(5)
             # Click on Synthesize Button
             self.driver.find_element_by_xpath(".//*[@id='AutoNumber1']/tbody/tr[4]/td/input[2]").click()
             time.sleep(5)
             # Right-Click on Output Link
             download = self.driver.find_element_by_xpath(".//*[@id='jsn-page']/center[1]/a")
             ActionChains(self.driver).context_click(download).key_down(Keys.ARROW_DOWN).perform()
-            time.sleep(2)
+            time.sleep(5)
             shell = win32com.client.Dispatch("WScript.Shell")
             shell.SendKeys('k')
             time.sleep(5)
@@ -51,9 +53,10 @@ class TTS(unittest.TestCase):
             shell.SendKeys(os.getcwd() + str1 + str(inputLineNum))
             time.sleep(5)
             shell.SendKeys('{ENTER}')
-            time.sleep(2)
+            time.sleep(11)
             # Click on Back
             self.driver.find_element_by_xpath(".//*[@id='jsn-page']/center[2]/a").click()
+            time.sleep(5)
 
     @classmethod
     def tearDownClass(cls):
