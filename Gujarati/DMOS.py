f86b42f191b3e0c3f4e3d773da35d39d894cfd67
Kan!skA <kaniska2008@gmail.com>
2015-05-13 00:13:05 +0530

    build4

--- Gujarati/DMOS.py
+++ Gujarati/DMOS.py
@@ -1,8 +1,9 @@
-__author__ = 'Kan!skA'
+__author__ = 'Kan!skA & Ashish'
 
 import unittest
 import csv
-import time, os
+import time
+import os
 import win32com.client
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -10,7 +11,6 @@ from selenium.webdriver.common.keys import Keys
 
 class TTS(unittest.TestCase):
     driver = None
-
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
@@ -24,13 +24,17 @@ class TTS(unittest.TestCase):
         set = "Set 2"
 
         rows = dict()
-        data_file = open(set + '/Sentences/DMOS/DMOS.csv', "r", encoding='utf-8')
+        inputFileName = set + '/Sentences/DMOS/DMOS'
+        data_file = open(inputFileName + '.csv', "r", encoding='utf-8')
         reader = csv.reader(data_file)
         lineNum = 1
+        lineNumForFile = 1
         for row in reader:
             rows[lineNum] = row
             lineNum += 1
         time.sleep(5)
+        errorOutputFileName = inputFileName + '_error'
+        error_out_data_file = open(errorOutputFileName + '.csv', "w", encoding='utf-8')
         for inputLineNum, inputData in rows.items():
             # Click on Clear Button
             self.driver.find_element_by_xpath(".//*[@id='AutoNumber1']/tbody/tr[4]/td/input[1]").click()
@@ -40,27 +44,37 @@ class TTS(unittest.TestCase):
             time.sleep(5)
             # Click on Synthesize Button
             self.driver.find_element_by_xpath(".//*[@id='AutoNumber1']/tbody/tr[4]/td/input[2]").click()
-            time.sleep(5)
-            # Right-Click on Output Link
-            download = self.driver.find_element_by_xpath(".//*[@id='jsn-page']/center[1]/a")
-            ActionChains(self.driver).context_click(download).key_down(Keys.ARROW_DOWN).perform()
-            time.sleep(5)
-            shell = win32com.client.Dispatch("WScript.Shell")
-            shell.SendKeys('k')
-            time.sleep(5)
-            # Path for Keeping Output
-            str1 = str('\Audio\\' + set + '\Sentences\DMOS\DMOS-' + set + '-')
-            shell.SendKeys(os.getcwd() + str1 + str(inputLineNum))
-            time.sleep(5)
-            shell.SendKeys('{ENTER}')
             time.sleep(11)
+            displayed = self.driver.find_elements_by_xpath(".//*[@id='jsn-page']/center[1]/a").__len__()
+
+            if displayed > 0:
+                # Right-Click on Output Link
+                download = self.driver.find_element_by_xpath(".//*[@id='jsn-page']/center[1]/a")
+                ActionChains(self.driver).context_click(download).key_down(Keys.ARROW_DOWN).perform()
+                time.sleep(5)
+                shell = win32com.client.Dispatch("WScript.Shell")
+                shell.SendKeys('k')
+                time.sleep(5)
+                # Path for Keeping Output
+                str1 = str('\Audio\\' + set + '\Sentences\DMOS\DMOS-' + set + '-')
+                shell.SendKeys(os.getcwd() + str1 + str(lineNumForFile))
+                lineNumForFile += 1
+                time.sleep(5)
+                shell.SendKeys('{ENTER}')
+                time.sleep(11)
+            else:
+                error_out_data_file.write(str(inputLineNum) + " : " + inputData[0])
+                error_out_data_file.writelines('\n')
+                time.sleep(3)
+
             # Click on Back
-            self.driver.find_element_by_xpath(".//*[@id='jsn-page']/center[2]/a").click()
+            self.driver.find_element_by_link_text("GoBack").click()
             time.sleep(5)
+        error_out_data_file.close()
 
     @classmethod
     def tearDownClass(cls):
-        cls.driver.quit()
+        cls.driver.close()
 
 if __name__ == '__main__':
     unittest.main()
